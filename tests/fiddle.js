2b1f2cb8dcc8db90de78d75c5fbbc9f79ff76ea2
Arjun Sunil <arjunsunil@tricog.com>
2019-09-05 00:29:39 +0530

    added dummy fiddle

--- tests/fiddle.js
+++ tests/fiddle.js
@@ -1,30 +1,24 @@
 let options = {
 	redisPort:6379,
 	redisHost: 'localhost',
-	topic: 'arn:aws:sns:us-east-2:316321551255:dummy_local',
-	awsAccessKeyId:'AKIAUTJR32OL4EOPFRNY',
-	awsSecretAccessKey: 'NYn9dDPwBon3pRfOMhbuL9FtHyVxN/PNs3CyJ2xz',
-	awsRegion: 'us-east-2',
+	topic: 'topic_arn',
+	awsAccessKeyId:'accesskey',
+	awsSecretAccessKey: 'secret',
+	awsRegion: 'region',
+	timerKey: 'tester'
 
 };
-// !options.awsAccessKeyId || !options.awsSecretAccessKey || !options.awsRegion
+
 let node_timer = require('../index');
 let nodeTimer = new node_timer(options) ;
 let moment = require('moment');
 
-// let key = 'Task3';
-// let time = new Date();
-// time = new Date().getTime();
-// console.log("time : ", time);
-// nodeTimer.addTimerEvent(key, time).then(() => {
-// 	console.log('successy success');	
-// }).catch(err => console.error("err ", err));
+let key = 'Task3';
+let time = new Date();
 
-// nodeTimer.processTimer().then(() => {
-// 	console.log('ok');
-// }).catch((err) => {
-// 	console.error("err ", err);
-// });
+nodeTimer.addTimerEvent(key, time).then(() => {
+	console.log('successfully added timer');	
+}).catch(err => console.error("err ", err));
 
 setInterval(async () => {
 	try{
